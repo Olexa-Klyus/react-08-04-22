dca0e21438109a7593627b5800f95b6de9631970
Olexa-Klyus <97458062+Olexa-Klyus@users.noreply.github.com>
2022-04-09 10:57:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import Actor from "./components/Actor";
 import Users from "./components/Users";
+import Actors from "./components/Actors";
 
 function App() {
 
-    const actorsArr = [{
-        id: 1,
-        name: 'Tom',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrIxNOzCSR_HnptRGmL8-E2JrxLrATpgS8tw&usqp=CAU'
-    },
-        {
-            id: 2,
-            name: 'jerry',
-            img: 'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'
-        }]
 
     return (
         <div>
@@ -23,25 +14,7 @@ function App() {
 
             //----------------------------------------------------
 
-            <div>
-                <h1>Tom</h1>
-                <img
-                    src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrIxNOzCSR_HnptRGmL8-E2JrxLrATpgS8tw&usqp=CAU'}
-                    alt={'Tom'}/>
-            </div>
-
-            <Actor/>
-            <div>
-                <h1>Jerry</h1>
-                <img
-                    src={'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'}
-                    alt={'Jerry'}/>
-            </div>
-
-            <Actor name={actorsArr[0].name}
-                   img={'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'}/>
-
-            {actorsArr.map(value => <Actor key={value.id} name={value.name} img={value.img}/>)}
+            <Actors/>
 
         </div>)
 }
---
+++ src/components/Actors.js
@@ -0,0 +1,42 @@
+import Actor from "./Actor";
+
+const Actors = () => {
+
+    const actorsArr = [{
+        id: 1,
+        name: 'Tom',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrIxNOzCSR_HnptRGmL8-E2JrxLrATpgS8tw&usqp=CAU'
+    },
+        {
+            id: 2,
+            name: 'jerry',
+            img: 'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'
+        }]
+
+    return (
+        <div>
+            <div>
+                <h1>Tom</h1>
+                <img
+                    src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrIxNOzCSR_HnptRGmL8-E2JrxLrATpgS8tw&usqp=CAU'}
+                    alt={'Tom'}/>
+            </div>
+
+            <Actor/>
+            <div>
+                <h1>Jerry</h1>
+                <img
+                    src={'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'}
+                    alt={'Jerry'}/>
+            </div>
+
+            <Actor name={actorsArr[0].name}
+                   img={'https://w7.pngwing.com/pngs/222/493/png-transparent-jerry-mouse-tom-cat-tom-and-jerry-actor-celebrities-mammal-heroes.png'}/>
+
+            {actorsArr.map(value => <Actor key={value.id} name={value.name} img={value.img}/>)}
+
+        </div>
+    );
+};
+
+export default Actors;
