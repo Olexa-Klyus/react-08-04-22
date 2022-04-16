691ba887a15f1bd33243ba6926a68d8f4aa9f595
olexa <olexakljus@gmail.com>
2022-04-16 13:37:42 +0300

    les 1 100%

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -22,7 +22,6 @@ const Users = () => {
     //     userService.getAll().then(({data}) =>setUsers(data))
     // },[])
 
-
     return (
         <div>
             {users.map(user=><User key={user.id} user={user}/>)}
