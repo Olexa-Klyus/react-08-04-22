3f45873e0989a89662b0cbf07a806196ea266e40
olexa <olexakljus@gmail.com>
2022-04-16 20:51:59 +0300

    les 3 form2

--- src/components/Form2/Form2.js
+++ src/components/Form2/Form2.js
@@ -1,9 +1,17 @@
+// контрольована форма
+import {useState} from "react";
+
 const Form2 = () => {
+    const [user, setUser] = useState({})
+
+    const getUser = (e) => {
+        setUser({...user,[e.target.name]:e.target.value})
+    }
     return (
         <div>
-            <div><label>Name:<input type="text"/></label></div>
-            <div><label>Age:<input type="number"/></label></div>
-
+            <div><label>Name:<input type="text" name={'name'} onChange={getUser}/></label></div>
+            <div><label>Age:<input type="number" name={'age'} onChange={getUser}/></label></div>
+            {JSON.stringify(user)}
         </div>
     );
 };
