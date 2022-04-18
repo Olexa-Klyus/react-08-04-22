1865f13a9666f78c4423936c9726f388c498511f
olexa <olexakljus@gmail.com>
2022-04-18 14:56:54 +0300

    les 4 route 50%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+// import {Button} from "./components";
+import {Routes, Route} from "react-router-dom";
+
 import './App.css';
+import {MainLayout} from "./layouts";
+import {AboutPage, HomePage, PostsPage, UsersPage} from "./pages";
 
 function App() {
+    // якщо в компоненті є ({children}) то якщо записати в App-ці
 
-  return (
-    <div>
+    // <div>
+    // <Button>Click</Button>
+    // </div>
 
-    </div>
-  );
+    // то все що прописано в компоненті Button ({children})
+    // попадає в тег Button
+    //______________________________________________________
+
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={'/home'} element = {<HomePage/>}/>
+                <Route path={'/users'} element = {<UsersPage/>}/>
+                <Route path={'/posts'} element = {<PostsPage/>}/>
+                <Route path={'/about'} element = {<AboutPage/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/constants/urls.js
@@ -0,0 +1,9 @@
+const base_URL = process.env.REACT_APP_API
+
+const urls ={
+    users:'/users',
+    posts:'posts'
+}
+
+export default base_URL
+export {urls}
