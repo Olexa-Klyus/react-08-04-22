7d3bc505947a23a6afc890e2c7b91c29a250b592
olexa <olexakljus@gmail.com>
2022-04-20 18:28:37 +0300

    les 4 route 50%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import {useReducer} from "react";
+
 import './App.css';
 
-function App() {
-
-  return (
-    <div>
-
-    </div>
-  );
+const init = (initCount) => {
+    return {count1: initCount, count2: initCount}
 }
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: 0}
+        case 'set' :
+            return {...state, count1: action.payload}
+        default:
+            return state
+    }
+
+}
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, 0, init);
+
+
+    return (
+        <div>
+            {state.count1}
+            <button onClick={() => dispatch({type: 'inc'})}>inc</button>
+            <button onClick={() => dispatch({type: 'dec'})}>dec</button>
+            <button onClick={() => dispatch({type: 'reset'})}>reset</button>
+            <button onClick={() => dispatch({type: 'set', payload:10})}>setTo10</button>
+        </div>
+    );
+}
+
 
 export default App;
