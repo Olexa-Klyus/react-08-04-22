26f05822dd57a381fb9f0cd56be4fd1167075e4c
olexa <olexakljus@gmail.com>
2022-04-22 14:45:57 +0300

    hw1

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,8 @@
+import './User.css'
+
 const User = ({user}) => {
     return (
-        <div>
+        <div className={user.id % 2 ? 'red':'green'}>
             <span>{user.id}</span>
             <span>{user.name}</span>
             <span>{user.email}</span>
